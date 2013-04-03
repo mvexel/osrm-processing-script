722cb2ea39964c50302eee71c5f7af23dd5a5012
Ubuntu <ubuntu@ip-10-149-19-135.ec2.internal>
2013-04-03 18:17:39 +0000

    fixes

--- download-process-osrm.sh
+++ download-process-osrm.sh
@@ -10,31 +10,44 @@ exec 1>/osm/log.out 2>&1
 dl=false
 sus=false
 filebasename="north-america"
+upd=false
 
-while getopts :df:sh opt
+while getopts :db:suh opt
 do
 	case "$opt" in
 		d) 
 			dl=true
 		;;
-		o)
+		b)
 			filebasename=$OPTARG
 		;;
 		s) 
 			sus=true
 		;;
+		u)
+			upd=true
+		;;
 		h)
-			echo "Usage:....read the source..."
+			echo "Usage:....read the source..." >&3
+			exit 0
 		;;
 		\?)
-			echo "Invalid option: -$OPTARG" >&2
+			echo "Invalid option: -$OPTARG" >&3
+			exit 1
 		;;
 	esac
 done
 
 filename="/osm/$filebasename.osm.pbf"
-echo "Using input file $filename"
-echo
+if [ -f $filename ]; then
+	echo "Using input file $filename" >&3
+	echo >&3
+else
+	echo "$filename does not exist." >&3
+	exit 1
+fi
+
+updatefilename="/osm/$filebasename-new.osm.pbf"
 
 echo "$(date) : ---------------- start ----------------" >&3
 echo "$(date) : ---------------- start ----------------"
@@ -46,11 +59,24 @@ echo "$(date) : set cwd to /osm"
 cd /osm || exit $?
 
 # download - this is hardcoded for N-America and Geofabrik right now. 
-if [ dl ]; then
+if $dl ; then
 	echo "$(date) : downloading from geofabrik" >&3
 	echo "$(date) : downloading from geofabrik"
 	wget http://download.geofabrik.de/north-america-latest.osm.pbf || exit $?
 	mv north-america-latest.osm.pbf north-america.osm.pbf
+fi
+
+#update osm data
+if $upd; then
+	#set cwd
+	cd /osm
+	echo "$(date) : update planet file" >&3
+	echo "$(date) : update planet file"
+	/osm/osmupdate $filename $updatefilename || exit $?
+	#overwrite old osm file
+	echo "$(date) : replace old planet file" >&3
+	echo "$(date) : replace old planet file"
+	mv $updatefilename $filename || exit $?
 fi
 
 #set cwd
@@ -66,7 +92,10 @@ echo "$(date) : osrm-prepare" >&3
 echo "$(date) : osrm-prepare"
 ./osrm-prepare /osm/$filebasename.osrm /osm/$filebasename.osrm.restrictions || exit $?
 
-if [ sus ]; then
+#finally create timestamp file:
+/osm/osmconvert --out-statistics /osm/utah-latest.osm.pbf | sed -rne "s/timestamp max: (.+)/\1/p" > /osm/$filebasename.osrm.timestamp || exit $?
+
+if $sus ; then
 	echo "$(date) : ---------------- end, halting ----------------" >&3
 	echo "$(date) : ---------------- end, halting ----------------"
 	cd /osm || exit $?
